ba5330e6a4178d8933a914a16491b80135f356ae
Bryan Ramos <bryan@ramos.codes>
2026-08-15 17:39:40 -0400

    render-markdown added for browser img support

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -13,4 +13,17 @@ return {
       { "<leader>mr", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Markdown Render" },
     },
   },
+  {
+    "selimacerbas/markdown-preview.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "selimacerbas/live-server.nvim",
+    },
+    config = function()
+      require("markdown_preview").setup()
+    end,
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Preview Markdown" },
+    },
+  },
 }
